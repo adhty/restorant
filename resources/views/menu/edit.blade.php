ee96debb03a79949f6300c3700967ae06e8777b6
enjoyyourcreaditcard <joywidiwibowo@gmail.com>
2024-11-29 16:07:20 +0700

    fix: Edit menu

--- resources/views/menu/edit.blade.php
+++ resources/views/menu/edit.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-2">
-       
+
         <div class="flex mt-6 justify-between items-center">
             <h2 class="font-semibold text-xl text-blue-600">Edit Menu</h2>
             @include('menu.delete')
         </div>
 
         <div class="mt-4" x-data="{ imageUrl: '/storage/{{$menu->foto}}' }">
-            <form enctype="multipart/form-data" method="POST" action="{{ route('menus.update', $menu) }}" class="flex gap-8">
+            <form enctype="multipart/form-data" method="POST" action="{{ route('menu.update', $menu) }}" class="flex gap-8">
                 @csrf
                 @method('PUT')
 
@@ -29,21 +29,21 @@
                     <div>
                         <x-input-label for="nama" :value="('Nama')" />
                         <x-text-input id="nama" class="block mt-1 w-full" type="text" name="nama"
-                            :value="$menus->nama" required />
+                            :value="$menu->nama" required />
                         <x-input-error :messages="$errors->get('nama')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="harga" :value="('Harga')" />
                         <x-text-input id="harga" class="block mt-1 w-full" type="text" name="harga"
-                            :value="$menus->harga" required />
+                            :value="$menu->harga" required />
                         <x-input-error :messages="$errors->get('harga')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="deskripsi" :value="('Deskripsi')" />
                         <x-text-area id="deskripsi" class="block mt-1 w-full" type="text"
-                            name="deskripsi">{{ $menus->deskripsi }}</x-text-area>
+                            name="deskripsi">{{ $menu->deskripsi }}</x-text-area>
                         <x-input-error :messages="$errors->get('deskripsi')" class="mt-2" />
                     </div>
 
